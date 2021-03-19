9047350ccfec23ef04191125304524793d30ab44
jaechang3456 <78472987+jaechang3456@users.noreply.github.com>
2021-03-19 10:06:08 +0900

    Python으로 MySQL 접속

--- app.py
+++ app.py
@@ -59,3 +59,104 @@ def main():
 
 if __name__ == '__main__' :
     main()
+
+# def main():
+
+#     name = st.text_input('이름을 입력하세요.')
+#     date = st.date_input('생년월일을 입력하세요.')
+#     time = st.time_input('태어난 시간을 입력하세요.')
+#     date_time = datetime.combine(date, time)
+ 
+#     # print(date)
+#     # print(time)
+#     # print(date_time)
+
+#     if st.button('저장'):
+
+#         try :
+#             connection = mysql.connector.connect(
+#                 host = 'database-1.cdxb0pb9vr70.us-east-2.rds.amazonaws.com',
+#                 database = 'yhdb',
+#                 user = 'streamlit',
+#                 password = '1q2w3e4r5t'
+#             )
+
+#             if connection.is_connected() :
+#                 print('연결됨')
+#                 db_info = connection.get_server_info()
+#                 print("MySQL sercer version : ", db_info)
+
+#                 cursor = connection.cursor()
+#                 # print('커서 가져옴')
+
+#                 query = '''insert into people (name, birthdate, birthtime, birthdt)
+#                         values (%s, %s, %s, %s);'''
+
+#                 record = (name, date, time, date_time)
+#                 print(datetime.now())
+                
+#                 cursor.execute(query, record)
+#                 connection.commit()
+
+#                 print('{}개 적용됨'.format(cursor.rowcount))
+
+#                 # record = cursor.fetchone()
+#                 # print('Connected to db : ', record)
+
+#         except Error as e :
+#             print('디비 관련 에러 발생' , e)
+
+#         finally :
+            
+#             cursor.close()
+#             connection.close()
+#             print('MySQL 커넥션 종료')
+
+
+# if __name__ == '__main__' :
+#     main()
+
+# def main():
+
+#     if st.button('저장'):
+
+#         try :
+#             connection = mysql.connector.connect(
+#                 host = 'database-1.cdxb0pb9vr70.us-east-2.rds.amazonaws.com',
+#                 database = 'yhdb',
+#                 user = 'streamlit',
+#                 password = '1q2w3e4r5t'
+#             )
+
+#             if connection.is_connected() :
+#                 print('연결됨')
+#                 db_info = connection.get_server_info()
+#                 print("MySQL sercer version : ", db_info)
+
+#                 cursor = connection.cursor()
+
+#                 query = '''insert into cats4 (name, age)
+#                         values (%s, %s);'''
+
+#                 record = [('냐옹이',1), ('나비', 3),('단비',5)]
+                
+#                 cursor.executemany(query, record)
+#                 connection.commit()
+
+#                 print('{}개 적용됨'.format(cursor.rowcount))
+
+#                 # record = cursor.fetchone()
+#                 # print('Connected to db : ', record)
+
+#         except Error as e :
+#             print('디비 관련 에러 발생' , e)
+
+#         finally :
+            
+#             cursor.close()
+#             connection.close()
+#             print('MySQL 커넥션 종료')
+
+
+# if __name__ == '__main__' :
+#     main()
